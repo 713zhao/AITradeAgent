ad11a86e29842eeda52500bdce07a8112657e81d
Eric Zhao <713.zhao@gmail.com>
2026-03-27 23:10:13 +0800

    chore: Remove temporary logs and scripts from root directory

--- fix_imports.py
+++
@@ -1,9 +0,0 @@
-import glob
-
-for fp in glob.glob("finance_service/**/*.py", recursive=True):
-    with open(fp, 'r') as f:
-        content = f.read()
-    if 'from finance_service.core.events import Event, Events, get_event_bus' in content:
-        content = content.replace('from finance_service.core.events import Event, Events, get_event_bus', 'from finance_service.core.event_bus import Event, Events, get_event_bus')
-        with open(fp, 'w') as f:
-            f.write(content)
